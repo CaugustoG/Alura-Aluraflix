671faba5febd59b3896c876c2428923a721c7ab9
César Gómez <caugustog89@gmail.com>
2024-07-04 00:14:17 -0500

    se crea companente boton y MiOrg

--- src/Components/ListaOpciones/Index.jsx
+++ src/Components/ListaOpciones/Index.jsx
@@ -1,16 +1,23 @@
 import "./ListaOpciones.css";
 
 const ListaOpciones = () =>{
+
+    const equipos = [
+        "Programación",
+        "Front End",
+        "Data Science",
+        "Devops",
+        "UX / UI",
+        "Móvil",
+        "Innovación y Gestión",
+    ]
+
     return <div className="lista-opciones">
         <label>Categoría</label>
         <select>
-            <option>Programación</option>
-            <option>Front End</option>
-            <option>Data Science</option>
-            <option>Devops</option>
-            <option>UX / UI</option>
-            <option>Móvil</option>
-            <option>Innovación y Gestión</option>
+            {equipos.map((equipo, index) => {
+                    return <option key={index}>{equipo}</option>
+            })}
         </select>
     </div>
 }
--- src/Components/Nuevo-video/index.jsx
+++ src/Components/Nuevo-video/index.jsx
@@ -1,18 +1,27 @@
 import "./NuevoVideo.css"
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones/Index"
+import Boton from "../Boton"
 
-const NuevoVideo = ()=>{
+const NuevoVideo = () => {
+
+    const manejarEnvio = (e) => {
+        e.preventDefault()
+        console.log("manejar envio", e)
+    }
+
     return <section className="nuevo-video">
-            <form>
-                <h2>nuevo video</h2>
-                <CampoTexto titulo="Titulo" placeholder ="Ingrese el titulo"/>
-                <ListaOpciones/>
-                <CampoTexto titulo="Imagen" placeholder ="Ingrega el enlace de la imagen"/>
-                <CampoTexto titulo="Video" placeholder ="Ingrega el enlace del video"/>
-                <CampoTexto titulo="Descripción" placeholder ="¿de qué se trata el video?"/>
-            </form>
-        </section>       
+        <form onSubmit={manejarEnvio}>
+            <h2>nuevo video</h2>
+            <CampoTexto titulo="Titulo" placeholder="Ingrese el titulo" required />
+            <ListaOpciones required />
+            <CampoTexto titulo="Imagen" placeholder="Ingrega el enlace de la imagen" required />
+            <CampoTexto titulo="Video" placeholder="Ingrega el enlace del video" required />
+            <CampoTexto titulo="Descripción" placeholder="¿de qué se trata el video?" required />
+            <Boton texto="Guardar" />
+            <Boton texto="Limpiar" />
+        </form>
+    </section>
 }
 
 export default NuevoVideo
