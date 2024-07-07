20c6167b7e7e64e22405247e803ef959c32f9626
César Gómez <caugustog89@gmail.com>
2024-07-06 19:34:28 -0500

    crecion componente Footer se agregan imagenes footer y logo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Header from "./Components/Header";
 import NuevoVideo from "./Components/Nuevo-video";
 import Categoria from "./Components/Categoria";
+import Footer from "./Components/Footer";
 
 function App() {
-  //HOOKS
+  //HOOKS actualizar video
   const [videos, actualizarVideos] = useState([]);
 
+  //Registro de videos
   const registrarVideo = (video) => {
     console.log("registrar video", registrarVideo);
     actualizarVideos([...videos, video]);
@@ -39,6 +41,7 @@ function App() {
       {categorias.map((categoria) => {
         return <Categoria datos={categoria} key={categoria.titulo} />;
       })}
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+
+const Footer = () => {
+    return <footer className="footer" style={{ backgroundImage: "url(/img/Footer.jpg)" }}>
+       
+        <img src="/img/LogoMain.png" alt="logo" className="logo"/>
+
+    </footer>
+}
+
+
+export default Footer
